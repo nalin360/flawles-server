f4db5ad2dc86697ab416de424b88bfca48718852
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2024-04-27 23:45:32 +0530

    feat: updated routes function

--- src/router/todoRouter.ts
+++ src/router/todoRouter.ts
@@ -13,6 +13,6 @@ export default (router: express.Router) => {
     router.post('/boards', createNewBoards);
     router.post('/todos', newWorkItem);
     router.get('/todos/boards/:boardId/work/:workType', getCurrentWorkItems);
-    router.put('/todos/boards/:boardId/work/:workType/itemID/:itemId', updateWorkItems);
-    router.delete('/todos:id', deleteWorkItems)
+    router.put('/todos/boards/', updateWorkItems);
+    router.delete('/todos/boards', deleteWorkItems)
 }
--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -1,15 +1,23 @@
 
-import  express  from "express";
-import { createBoardToUser, deleteUser, getAllUser, getUserById, getUserWithAllBoards, updateUser } from '../controllers/userController';
+import express from "express";
+import {
+    createBoardToUser,
+    deleteUser,
+    getAllUser,
+    getBoardbyID,
+    getUserById,
+    getUserWithAllBoards,
+    updateUser
+} from '../controllers/userController';
 import { isAuthenticated, isOwner } from '../middleware/index';
 
 
-export default (router:express.Router) => {
+export default (router: express.Router) => {
     router.get('/user', isAuthenticated, getAllUser);
-    router.delete('/user/:id', isAuthenticated,isOwner, deleteUser);   
-    router.patch('/user/:id', isAuthenticated,isOwner, updateUser);   
-    router.post('/user', createBoardToUser);   
+    router.delete('/user/:id', isAuthenticated, isOwner, deleteUser);
+    router.patch('/user/:id', isAuthenticated, isOwner, updateUser);
+    router.post('/user', createBoardToUser);
     // 
-    router.post('/user/boards', getUserWithAllBoards);   
-    router.post('/user/id', getUserById);   
+    router.post('/user/boards', getUserWithAllBoards);
+    router.post('/user/Boardbyid', getBoardbyID);
 }
